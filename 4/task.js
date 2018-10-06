c32d54bf9385fba818e6e878e527a2d6398ba92e
AntonVoronezh <pstut@mail.ru>
2018-10-06 18:49:24 +0300

    4 #6

--- 4/task.js
+++ 4/task.js
@@ -1,18 +1,15 @@
 import { isEmpty, head, tail, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
 import { value, is, toString as htmlToString } from 'hexlet-html-tags'; // eslint-disable-line
 
-// BEGIN (write your solution here)
 export const reduce = (fn, acc, elements) => {
+  const iter = (items, accIter) => (
+    isEmpty(items) ? accIter : iter(tail(items), fn(head(items), accIter))
+  );
 
-  const iter = (items, accIter) => 
-  isEmpty(items) ? accIter : iter(tail(items), fn(head(items), accIter));
-
-return iter(elements, acc);
+  return iter(elements, acc);
 };
 
 export const emptyTagsCount = (tag, elements) => {
-const fn = (element, acc) => (is(tag, element) && value(element) === '' ? acc + 1 : acc)
-return reduce(fn, 0, elements);
+  const fn = (element, acc) => (is(tag, element) && value(element) === '' ? acc + 1 : acc);
+  return reduce(fn, 0, elements);
 };
-
-
