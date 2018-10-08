22f078f4f7fed76dd8572cd590e8ee57c8c8dd8f
AntonVoronezh <pstut@mail.ru>
2018-10-08 18:41:19 +0300

    7 #6

--- 7/task.js
+++ 7/task.js
@@ -1,9 +1,14 @@
-import { l, isEmpty, head, tail, filter, toString as listToString } from 'hexlet-pairs-data';
+import {
+  l, isEmpty, head, tail, filter, toString as listToString,
+} from 'hexlet-pairs-data';
 
-// BEGIN (write your solution here)
-const sameParity = list => {
-if (isEmpty(list)) {return l();}
-else return head(list) % 2 === 0 ? filter(element => element % 2 === 0, list) : filter(element => element % 2 !== 0, list)
+const sameParity = (list) => {
+  if (isEmpty(list)) {
+    return l();
+  }
+  return head(list) % 2 === 0 ? filter(
+    element => element % 2 === 0, list,
+  ) : filter(element => element % 2 !== 0, list);
 };
 
 export default sameParity;
