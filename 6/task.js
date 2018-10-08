9eb6a34d0d2edfd57592dc74d8a441a184fe2d7e
AntonVoronezh <pstut@mail.ru>
2018-10-08 18:36:38 +0300

    6 #6

--- 6/task.js
+++ 6/task.js
@@ -1,19 +1,19 @@
-import { l, cons as consList, isList, isEmpty, head, tail, concat, toString as listToString } from 'hexlet-pairs-data';
-// eslint-disable-next-liconsListne
-import { is, toString as htmlToString, hasChildren, children, filter, reduce } from 'hexlet-html-tags';
-
-// BEGIN (write your solution here)
+import {
+  l, cons as consList, isList, isEmpty, head, tail, concat, toString as listToString,
+} from 'hexlet-pairs-data';
+import {
+  is, toString as htmlToString, hasChildren, children, filter, reduce,
+} from 'hexlet-html-tags';
 
 const select = (tag, tree) => {
-   const fn = (element, acc) => {
-    if (!hasChildren(element)) { 
+  const fn = (element, acc) => {
+    if (!hasChildren(element)) {
       return is(tag, element) ? concat(l(element), acc) : acc;
-       } else {
-      const accChildren = is(tag, element) ? concat(children(element), acc) : acc;
-      return reduce(fn, accChildren, children(element));
     }
-  }
-return reduce(fn, l(), tree);
+    const accChildren = is(tag, element) ? concat(children(element), acc) : acc;
+    return reduce(fn, accChildren, children(element));
+  };
+  return reduce(fn, l(), tree);
 };
 
 export default select;
