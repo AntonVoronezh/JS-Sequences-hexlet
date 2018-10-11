84622de994fd9223baf99dfffb28fc997729a1cc
AntonVoronezh <pstut@mail.ru>
2018-10-11 12:34:48 +0300

    8 #4

---
+++ 8/task.js
@@ -0,0 +1,13 @@
+import { l, isEmpty, head, tail, cons, reverse, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
+
+// BEGIN (write your solution here)
+export default (list1, list2) => {
+  const iter = (list1Iter, list2Iter, accIter) => {
+    if (isEmpty(list1Iter) || isEmpty(list2Iter)) {
+      return reverse(accIter);
+    }
+    accIter =  cons(l(head(list1Iter), head(list2Iter)), accIter);
+    return iter(tail(list1Iter), tail(list2Iter), accIter);
+  }
+return iter(list1, list2, l());
+};
