1b5b546b3a1eef3bffbe792e50ccf25e1f46efec
AntonVoronezh <pstut@mail.ru>
2018-10-14 15:38:45 +0300

    9 #6

--- 9/task.js
+++ 9/task.js
@@ -1,14 +1,15 @@
 // eslint-disable-next-line
 import { l, isEmpty, head, tail, cons, toString as listToString } from 'hexlet-pairs-data';
 
-// BEGIN (write your solution here)
 const take = (num, list) => {
-const iter = (listIter, accList, accNum) => {
+  const iter = (listIter, accList, accNum) => {
     if (isEmpty(listIter)) {
       return listIter;
     }
-    return num === accNum ? accList : cons(head(listIter), iter(tail(listIter), accList, accNum + 1));
+    return num === accNum ? accList : cons(
+      head(listIter), iter(tail(listIter), accList, accNum + 1),
+    );
   };
-return iter(list, l(), 0);
+  return iter(list, l(), 0);
 };
 export default take;
