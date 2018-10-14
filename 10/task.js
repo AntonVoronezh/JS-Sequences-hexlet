c477f4f8bfe4e90e09bdcfffb732aff99a0e4e88
AntonVoronezh <pstut@mail.ru>
2018-10-14 15:42:18 +0300

    10 #6

--- 10/task.js
+++ 10/task.js
@@ -1,14 +1,15 @@
-import { l, isEmpty, cons, reduce, has, reverse, toString as listToString } from 'hexlet-pairs-data';
+import {
+  l, isEmpty, cons, reduce, has, reverse, toString as listToString,
+} from 'hexlet-pairs-data';
 
-// BEGIN (write your solution here)
 const union = (list1, list2) => {
   const fn = (element, acc) => {
     if (isEmpty(l(element))) {
       return acc;
     }
-  return has(acc, element) ? acc: cons(element, acc);
+    return has(acc, element) ? acc : cons(element, acc);
   };
-return reverse(reduce(fn, reduce(fn, l(), list1), list2));
+  return reverse(reduce(fn, reduce(fn, l(), list1), list2));
 };
 
 export default union;
