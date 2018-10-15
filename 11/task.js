bdd95529db487dc2180d2e92404f3caa6e162d6a
AntonVoronezh <pstut@mail.ru>
2018-10-15 20:15:14 +0300

    11 #5

---
+++ 11/task.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line
+import { l, isEmpty, reverse, toString as listToString, isList, head, tail, cons, reduce } from 'hexlet-pairs-data';
+
+// BEGIN (write your solution here)
+const flatten = list => {
+  const fn = (element, acc) => {
+    if (isEmpty(l(element))) {
+      return acc;
+    }
+  return isList(element) ? reduce(fn, acc, element) : cons(element, acc);
+  };
+return reverse(reduce(fn, l(), list));
+};
+
+export default flatten;
